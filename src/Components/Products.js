6a79f37dd28e767829ea6d2c0cdcf973c3564295
rdssurya <rdssurya@gmail.com>
2023-09-12 21:22:55 +0530

    setting up Filter component

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Grid } from "@mui/material";
+import { Grid, Typography, Hidden } from "@mui/material";
 import axios from "axios";
 import Header from "./Header";
 import ProductCard from "./ProductCard";
-
+import '../Styles/Products.css';
+import { ArrowDropDown } from "@mui/icons-material";
 
 // Definition of Data Structures used
 /**
@@ -43,7 +44,7 @@ export default function Products(){
      * 
      *  @returns { Array.<Product> }
      *  Array of objects with complete data on all available products
-     *  Adds the details of all available products to localStorage as well
+     *  Stores the details of all available products to localStorage as well
      * 
      * API endpoint - GET 'https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/catalogue.json'
      * 
@@ -60,14 +61,56 @@ export default function Products(){
         }
     }
 
+
+    /**
+     * Definition of handleChange()
+     * Function which is called if there is change in search bar
+     * @param {string} text
+     *      Text is taken from the user and is used to filter the products 
+     * If text is an empty string AllProducts from the local storage will be shown
+     * In other cases products whose name includes the given text will be filtered and will be shown
+     * @returns {Array.<Product>} 
+     * Updates the products array
+     */
+    const captureTheInputValue = (text) => {
+        const allAvailableProducts = [...JSON.parse(localStorage.getItem('AllProducts'))]
+        if(text===''){
+            setProducts([...allAvailableProducts]);
+        }
+        else{
+            const givenInputWord = text.trim();
+            const filteredProducts = allAvailableProducts.filter((product) => {
+                return product.name.toLowerCase().includes(givenInputWord.toLowerCase());
+            });
+            setProducts([...filteredProducts]);
+        }    
+    }
+
+
     return (
         <>
         <Header/>
+        <div className="text-field">
+            <div>
+                <input 
+                    type="text" 
+                    onChange={(e)=> captureTheInputValue(e.target.value) } 
+                    placeholder="Search (Ex: Polo)"
+                />
+            </div>
+        </div>
         <Grid container>
-            <Grid item xs={3}>
-
+            <Grid item md={3}>
+                <Hidden smDown>
+                    <Typography>Hello</Typography>
+                </Hidden>
+                <Hidden>
+                    <ArrowDropDown>Open</ArrowDropDown>
+                </Hidden>
             </Grid>
-            <Grid item xs={9}>
+            {products.length === 0 ? 
+            <h2>Sorry! No Products found. Try searching for any other product... </h2>:
+            <Grid item md={9}>
                 <Grid container spacing={3}>
                     {products.map((product)=>(
                         <Grid item key={product.id} xs={12} sm={4} md={3}>
@@ -81,7 +124,7 @@ export default function Products(){
                         </Grid>
                     ))}
                 </Grid>
-            </Grid>
+            </Grid>}
         </Grid>
         </>
     );
